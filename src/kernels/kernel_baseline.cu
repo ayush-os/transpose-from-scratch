de42f4a5a3a2bde440e03039ef08611e6e870821
ayush <99374392+ayush-os@users.noreply.github.com>
2026-02-07 12:43:04 -0800

    nit

--- src/kernels/kernel_baseline.cu
+++ src/kernels/kernel_baseline.cu
@@ -1,12 +1,5 @@
 #include <math.h>
 
-const int TILE_DIM = 32;
-const int BLOCK_ROWS = 8;
-
-__global__ void baseline_copy_kernel(float *output, const float *input)
-{
-}
-
 // --- Baseline Kernel
 
 __global__ void baseline_transpose_kernel(float *output, const float *input)
