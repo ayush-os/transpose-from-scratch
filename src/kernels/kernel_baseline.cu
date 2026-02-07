b22d7bbab4c3bd3d10761bd9b7e711aaed6db22e
ayush <99374392+ayush-os@users.noreply.github.com>
2026-02-07 13:46:21 -0800

    smem v1

--- src/kernels/kernel_baseline.cu
+++ src/kernels/kernel_baseline.cu
@@ -2,12 +2,25 @@
 
 // --- Baseline Kernel
 
+const int TILE_SIZE = 32;
+
 __global__ void baseline_transpose_kernel(float *output, const float *input)
 {
+  __shared__ float tile[TILE_SIZE][TILE_SIZE];
+
   int x = blockIdx.x * blockDim.x + threadIdx.x;
   int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-  if (x < 4096 && y < 4096) {
-    output[x * 4096 + y] = input[y * 4096 + x];
-  }
+  tile[threadIdx.y][threadIdx.x] = input[y * 4096 + x];
+
+  __syncthreads();
+
+  int xNew = blockIdx.y * blockDim.x + threadIdx.x;
+  int yNew = blockIdx.x * blockDim.y + threadIdx.x;
+
+  output[yNew * 4096 + xNew] = tile[threadIdx.x][threadIdx.y];
+
+  // if (x < 4096 && y < 4096) {
+  //   output[x * 4096 + y] = input[y * 4096 + x];
+  // }
 }
