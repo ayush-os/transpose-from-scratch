dd94ad161ac0062f93919bf4aa493de3f2d3b109
ayush <99374392+ayush-os@users.noreply.github.com>
2026-02-07 12:53:45 -0800

    nit

--- src/kernels/kernel_baseline.cu
+++ src/kernels/kernel_baseline.cu
@@ -9,7 +9,7 @@ __global__ void baseline_transpose_kernel(float *output, const float *input)
 
   int threadIdYMatrix = threadIdY * 4;
 
-  if (threadIdX % 10) printf("%d\n", threadIdYMatrix);
+  if (threadIdX % 10000) printf("%d\n", threadIdYMatrix);
 
   output[threadIdYMatrix*4096+threadIdX] = input[threadIdX*4096+threadIdYMatrix];
   output[(threadIdYMatrix+1)*4096+threadIdX] = input[threadIdX*4096+threadIdYMatrix+1];
