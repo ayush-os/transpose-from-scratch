309739cd778bf00a58482347292dd946564e77bd
ayush <99374392+ayush-os@users.noreply.github.com>
2026-02-07 12:43:16 -0800

    baseline transpose kernel attempt 1

--- src/kernels/kernel_baseline.cu
+++ src/kernels/kernel_baseline.cu
@@ -4,5 +4,13 @@
 
 __global__ void baseline_transpose_kernel(float *output, const float *input)
 {
-  
+  int threadIdX = blockIdx.x * blockDim.x + threadIdx.x;
+  int threadIdY = blockIdx.y * blockDim.y + threadIdx.y;
+
+  int threadIdYMatrix = threadIdY * 4;
+
+  output[threadIdYMatrix][threadIdX] = input[threadIdX][threadIdYMatrix];
+  output[threadIdYMatrix+1][threadIdX] = input[threadIdX][threadIdYMatrix+1];
+  output[threadIdYMatrix+2][threadIdX] = input[threadIdX][threadIdYMatrix+2];
+  output[threadIdYMatrix+3][threadIdX] = input[threadIdX][threadIdYMatrix+3];
 }
