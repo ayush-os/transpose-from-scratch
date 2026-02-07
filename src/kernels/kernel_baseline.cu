cca4ae378f84ed334fa86e9d276408ea850d1adc
ayush <99374392+ayush-os@users.noreply.github.com>
2026-02-07 12:56:39 -0800

    massive simplification

--- src/kernels/kernel_baseline.cu
+++ src/kernels/kernel_baseline.cu
@@ -4,15 +4,10 @@
 
 __global__ void baseline_transpose_kernel(float *output, const float *input)
 {
-  int threadIdX = blockIdx.x * blockDim.x + threadIdx.x;
-  int threadIdY = blockIdx.y * blockDim.y + threadIdx.y;
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-  int threadIdYMatrix = threadIdY * 4;
-
-  if (threadIdX % 10000) printf("%d\n", threadIdYMatrix);
-
-  output[threadIdYMatrix*4096+threadIdX] = input[threadIdX*4096+threadIdYMatrix];
-  output[(threadIdYMatrix+1)*4096+threadIdX] = input[threadIdX*4096+threadIdYMatrix+1];
-  output[(threadIdYMatrix+2)*4096+threadIdX] = input[threadIdX*4096+threadIdYMatrix+2];
-  output[(threadIdYMatrix+3)*4096+threadIdX] = input[threadIdX*4096+threadIdYMatrix+3];
+  if (x < 4096 && y < 4096) {
+    output[x * 4096 + y] = input[y * 4096 + x];
+  }
 }
